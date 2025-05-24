a017bbd96a9e11eb0b08c8caa27d065082381afa
Bagusdevaa <deva999official@gmail.com>
2025-05-24 23:14:18 +0800

    fix: removes the auto reloader

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -1,5 +1,5 @@
 from app import create_app
-from flask import Blueprint, request, jsonify
+from flask import Blueprint, request, jsonify, render_template
 from app.models.user import User
 from app.response import success_response, error_response
 from app import db
@@ -7,11 +7,11 @@ from app.controller import usercontroller
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/register', methods=['POST'])
-def register():
-    return usercontroller.register()
+# @auth.route('/register', methods=['POST'])
+# def register():
+#     return usercontroller.register()
 
 
-@auth.route('/login', methods=['POST'])
-def login():
-    pass
+# @auth.route('/login', methods=['POST'])
+# def login():
+#     pass
