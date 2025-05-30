10e8e7d7d3bbbc40da3b8ca351b46bce8434a1e9
Bagusdevaa <deva999official@gmail.com>
2025-05-30 12:28:26 +0800

    feat: added auth logic to user controller

--- app/controller/usercontroller.py
+++ app/controller/usercontroller.py
@@ -1,8 +1,10 @@
-from flask import request
+from flask import request, session
 from flask_jwt_extended import create_access_token, create_refresh_token
+from flask_login import login_user
 from app.models.user import User
 import os
 from app import response, db
+from datetime import datetime, timedelta, timezone
 
 def singleobject(data):
     data = {
@@ -43,8 +45,37 @@ def register():
     except Exception as e:
         return response.error_response(str(e))
 
-# def login():
-#     try:
-#         pass
-#     except Exception as e:
-#         pass
+def login():
+    try:
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        user = User.query.filter_by(email=email).first()
+        if not user:
+            return response.error_response("User not found", 404)
+        
+        if not user.check_password(password):
+            return response.error_response("Invalid password", 401)
+        
+        data = singleobject(user)
+
+        expires = datetime.timedelta(days = 1)
+        expires_refresh = datetime.timedelta(days = 10)        # Use user ID as the identity (subject for the token)
+        access_token = create_access_token(identity=str(user.id), expires_delta=expires)
+        refresh_token = create_refresh_token(identity=str(user.id), expires_delta=expires_refresh)
+        
+        # Update last login timestamp
+        user.last_login = datetime.now(timezone.utc)
+        db.session.commit()
+        
+        # Log in the user with Flask-Login
+        login_user(user)
+        
+        return response.success_response({
+            'data' : data,
+            'access_token' : access_token,
+            'refresh_token' : refresh_token,
+        }, 'Login successful', 200)
+
+    except Exception as e:
+        return response.error_response(str(e))
